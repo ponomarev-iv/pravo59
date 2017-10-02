e85d33a3f9ff0085a2bdb89a417326affb9e6413
ponomarev-iv <ponomarev-iv@galagodigital.ru>
2017-10-02 15:21:52 +0500

    Добавил кнопку прокрутки и патерн для hero блоква

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -14,11 +14,24 @@ function mobileMenu(){
         $mobileBtn.removeClass('open');
         $mobileMenu.removeClass('is-open');
     })
+}
 
+function scrollDown(){
+    var scrollBtn = $('.js-scroll-down');
 
+    if(scrollBtn.length){
+        scrollBtn.on('click', function(e){
+            e.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top;
 
+            $('body, html').animate({scrollTop: top}, 1000);
+        })
+    }
+    else return false;
 }
 
 $(document).ready(function(){
     mobileMenu();
+    scrollDown();
 });
