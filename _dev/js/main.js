0ffbe4c9c8b65695b3f42c4dddc5363c47977f67
ponomarev86 <ilya86ponomarev@gmail.com>
2017-10-03 04:14:51 +0500

    Сделал секцию услуг и контента + адаптация к ней

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -5,20 +5,49 @@ function mobileMenu(){
     var $mobileBtn = $('.js-menu'),
         $mobileMenu = $('.js-mobile-menu');
 
-    $mobileBtn.click(function(){
+    $mobileBtn.on('click', function(){
         $(this).toggleClass('open');
         $mobileMenu.toggleClass('is-open');
     });
 
-    $('#page-wrap').click(function(){
+    $('#page-wrap').on('click', function(){
         $mobileBtn.removeClass('open');
         $mobileMenu.removeClass('is-open');
     })
+}
 
+function scrollDown(){
+    var scrollBtn = $('.js-scroll-down');
 
+    if(scrollBtn.length){
+        scrollBtn.on('click', function(e){
+            e.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top - 90;
 
+            $('body, html').animate({scrollTop: top}, 1000);
+        })
+    }
+    else return false;
+}
+
+//animation hero banner
+function animateHero(){
+    var header = $('.header'),
+        heroTitle = $('.hero__title'),
+        heroBtn = $('.hero__btn'),
+        heroList = $('.hero__list li'),
+        tl = new TimelineLite();
+
+    tl
+        .from(header, .6, {y: -35, autoAlpha: 0, ease:Power1.easeOut})
+        .from(heroTitle, 0.4, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
+        .from(heroList, 0.4, {x: -50, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
+        .from(heroBtn, 0.4, {y: 20, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15');
 }
 
 $(document).ready(function(){
     mobileMenu();
+    scrollDown();
+    animateHero();
 });
